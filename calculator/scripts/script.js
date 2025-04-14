cbf9ed9d92a40c3ee52c4013c56e2aa02acd6445
annmarytom <annmarytom9@gmail.com>
2025-04-14 14:28:22 +0400

    basic calulation operations added

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -7,3 +7,57 @@ setInterval(() => {
     time.innerText = `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}`;
 }, 1000);
 
+
+const themeButton = document.getElementById("toggle-btn");
+themeButton.addEventListener('click', () => {
+    darkmodeStatus = localStorage.getItem('darkmode');
+    darkmodeStatus !== "active" ? enableDarkMode() : disableDarkMode();
+
+})
+
+const disableDarkMode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmodeStatus', null);
+}
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmodeStatus', 'active');
+}
+
+
+const keys = document.querySelectorAll("button[data-key]");
+const displayHistory = document.querySelector(".calc-display .calc-display-history");
+const displayOutput = document.querySelector(" .calc-display .calc-display-value");
+
+let userInput = "";
+
+for (let key of keys) {
+    const value = key.dataset.key;
+    key.addEventListener("click", () => {
+        console.log(value);
+        console.log(displayOutput, "input");
+        console.log(displayHistory, "history");
+
+        if (value === "clear") {
+            userInput = "";
+            displayHistory.innerHTML = "";
+            displayOutput.innerHTML = "";
+        } else if (value === "backspace") {
+            userInput = userInput.slice(0, -1);
+            displayOutput.innerHTML = userInput;
+        } else if (value === "=") {
+            try {
+                const result = eval(userInput);
+                displayHistory.innerHTML = userInput;
+                displayOutput.innerHTML = result;
+                userInput = result.toString();
+            } catch {
+                displayOutput.innerHTML = "Error";
+            }
+        } else {
+            userInput += value;
+            displayOutput.innerHTML = userInput;
+        }
+    })
+}
